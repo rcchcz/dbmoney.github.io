814b5d2ed74eadf9f14d19cfea36b91f48c01cba
rcchcz <rcchcz@gmail.com>
2022-07-17 17:13:30 -0300

    boleto ok

---
+++ web/app/app.js
@@ -0,0 +1,21 @@
+function download_boleto() {
+    var boleto = generate_boleto()
+    var filename = "boleto.txt"
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(boleto));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+}
+
+function generate_boleto() {
+    var code = "";
+    for(var i = 0; i < 20; i++) { code += i; }
+    return code;
+}
+  
