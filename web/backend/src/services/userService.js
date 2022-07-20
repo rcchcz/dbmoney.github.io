576003842e2723ebe804f64c7e62993911ce5ee3
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-07-20 08:14:03 -0300

    resolvendo conflito

--- web/backend/src/services/userService.js
+++ web/backend/src/services/userService.js
@@ -4,51 +4,56 @@ const DbConnection = require('../database/connection')
 class UserService {
     async insertUser(userReceived) {
         try {
-            const database = await DbConnection();
-            const passwordEncrypted = await Bcrypt.hashingPassword(userReceived.senha);
-            const insertQuery = 'INSERT INTO Cliente (cliente_nome, cliente_email, cliente_telefone, ' +
+            const database = await DbConnection()
+            const passwordEncrypted = await Bcrypt.hashingPassword(userReceived.cliente_senha)
+            const insertQuery = 'INSERT INTO Cliente (cliente_nome,cliente_email, cliente_telefone, ' +
                 'cliente_cpf, cliente_endereco, cliente_data_nascimento, ' +
-                'cliente_senha, cliente_id_gerente) VALUES (?,?,?,?,?,?,?,?);';
-            const values = [userReceived.nome, userReceived.email, userReceived.telefone, userReceived.cpf, 
-                userReceived.endereco, userReceived.data_nascimento, 
-                passwordEncrypted, 1];
+                'cliente_senha, cliente_id_gerente) VALUES (?,?,?,?,?,?,?,?);'
+            const values = [userReceived.cliente_nome, userReceived.cliente_email,userReceived.cliente_telefone, userReceived.cliente_cpf, 
+                userReceived.cliente_endereco, userReceived.cliente_data_nascimento, 
+                passwordEncrypted, 1]
             await database.query(insertQuery, values);
-        } catch (err) {
-            return err;
+        } catch (error) {
+            console.log(error);
+            return error
         }
     }
     async getAllUsers() {
         try {
-            const database = await DbConnection();
-            const [usuarios] = await database.query('SELECT * FROM Cliente;');
-            return usuarios;
-        } catch (err) {
-            return err;
+            const database = await DbConnection()
+            const [usuarios] = await database.query('SELECT * FROM Cliente;')
+            return usuarios
+        } catch (error) {
+            console.log(error);
+            return error
         }
     }
     async getUserById(id) {
         try {
-            const database = await DbConnection();
-            const [usuarios] = await database.query('SELECT * FROM Cliente WHERE cliente_id=?;', id);
+            const database = await DbConnection()
+            const [usuarios] = await database.query('SELECT * FROM Cliente WHERE cliente_id=?;', id)
             if (usuarios.length > 0) {
-                return usuarios[0];
+                return usuarios[0]
             }
-        } catch (err) {
-            return err;
+        } catch (error) {
+            console.log(error);
+            return error
         }
     }
     async updateUser(id, userReceived) {
         try {
             const database = await DbConnection();
-            const updateQuery = 'UPDATE Cliente SET cliente_nome=?, cliente_email=?, cliente_telefone=?, ' +
+            const passwordEncrypted = await Bcrypt.hashingPassword(userReceived.cliente_senha)
+            const updateQuery = 'UPDATE Cliente SET cliente_nome=?,cliente_email=?, cliente_telefone=?, ' +
                 'cliente_cpf=?, cliente_endereco=?, cliente_data_nascimento=?, ' +
                 'cliente_senha=?, cliente_id_gerente=? WHERE cliente_id=?;';
-            const values = [userReceived.nome, userReceived.email, userReceived.telefone, userReceived.cpf, 
-                userReceived.endereco, userReceived.data_nascimento, 
-                userReceived.senha, 1, id];
+            const values = [userReceived.cliente_nome, userReceived.cliente_email, userReceived.cliente_telefone, userReceived.cliente_cpf, 
+                userReceived.cliente_endereco, userReceived.cliente_data_nascimento, 
+                userReceived.cliente_senha, 1, id];
             await database.query(updateQuery, values);
-        } catch (err) {
-            return err;
+        } catch (error) {
+            console.log(error);
+            return error;
         }
     }
     async changePassword(newPassword, id) {
@@ -58,16 +63,18 @@ class UserService {
             const updateQuery = 'UPDATE Cliente SET cliente_senha=? WHERE cliente_id=?;';
             const values = [passwordEncrypted, id];
             await database.query(updateQuery, values);
-        } catch (err) {
-            return err;
+        } catch (error) {
+            console.log(error);
+            return error;
         }
     }
     async deleteUser(id) {
         try {
             const database = await DbConnection();
             await database.query('DELETE FROM Cliente WHERE cliente_id=?;', id);
-        } catch (err) {
-            return err;
+        } catch (error) {
+            console.log(error);
+            return error;
         }
     }
     async validateCredentials(cpf, senha) {
@@ -78,12 +85,58 @@ class UserService {
             
             if (userFound.length > 0) {
                 if (await Bcrypt.comparePassword(senha, userFound[0].cliente_senha))
-                    return credentialsStatus = { status: true, msg: 'Credenciais válidas!', id: userFound[0].cliente_id };
+                    return credentialsStatus = { status: true, msg: 'Credenciais válidas!', id: userFound.cliente_id };
             } 
             
             return credentialsStatus = { status: false, err: 'Usuário e/ou senha incorretos!' };
         } catch (err) {
             return err;
+        }
+    }
+    async getUserIdByCPF(cpf){
+        try {
+            const database = await DbConnection();
+            const [userFound] = await database.query('SELECT * FROM Cliente WHERE cliente_cpf=?;',cpf);
+            console.log("Quantidade Result Returned -> " + userFound.length);
+            console.log(cpf);
+            return userFound[0].cliente_id;
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    }
+
+    async getAllUsersByTitularId(idTitulares){
+        try {
+            const database = await DbConnection()
+            const Users = [];
+            for (let id of idTitulares) {
+                Users.push(id.titular_id);
+            }
+            const joinedUsers = Users.join(',');
+            const query = 'select * from cliente where cliente_id in ('+joinedUsers+')';
+            const [result] = await database.query(query);
+            return result;
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    }
+
+    async getAllUsersByDependentId(idDependents){
+        try {
+            const database = await DbConnection()
+            const Users = [];
+            for (let id of idDependents) {
+                Users.push(id.dependente_id);
+            }
+            const joinedUsers = Users.join(',');
+            const query = 'select * from cliente where cliente_id in ('+joinedUsers+')';
+            const [result] = await database.query(query);
+            return result;
+        } catch (error) {
+            console.log(error);
+            return error;
         }
     }
 }
