a17719655c2f53e435c9066d731fe4119328f359
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2022-07-20 02:35:16 -0300

    adicionando autenticação

---
+++ web/app/authentication.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', event => isAuthenticated(event));
+function isAuthenticated(event) {
+    event.preventDefault();
+    let idClient = sessionStorage.getItem('cliente_id');
+    if(idClient) {
+        getClient(idClient);
+    } else {
+        alert("Usuário não autenticado!");
+        document.location.href = 'index.html';
+    }
+}
+
+function logOut() {
+    localStorage.removeItem('cliente_id');
+    document.location.href = 'index.html';
+}
+
+function getClient(id) {
+    let request = new XMLHttpRequest();
+    request.open('GET', `http://localhost:3000/user/${id}`, true);
+    request.send();
+    request.onload  = function() {
+        if (this.status == 200) {
+            let responseData = JSON.parse(request.response);
+            sessionStorage.setItem('cliente_nome', responseData.cliente_nome);
+        } 
+    };
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    event.preventDefault();
+    let idClient = sessionStorage.getItem('cliente_id');
+    if(idClient) {
+        renderSidebar();
+    }
+})
+
+function renderSidebar() {
+    // icone do cliente
+    let userAvatar = document.querySelector('.user-pic');
+    userAvatar.innerHTML = sessionStorage.getItem('cliente_nome')[0];
+
+    // nome do cliente
+    let userName = document.querySelector('.user-name');
+    userName.innerHTML = sessionStorage.getItem('cliente_nome');
+}
