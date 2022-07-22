cd7b2ac4debf12a94ffa34f6d8ef54026f91be6b
Paulo Vitor Fernandes Andrade <69232148+PauloFAndrade@users.noreply.github.com>
2022-07-21 21:17:48 -0300

    Update connection.js

--- web/backend/src/database/connection.js
+++ web/backend/src/database/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise")
 async function connect(){
     if(global.connection && global.connection.state !== 'disconnected')
         return global.connection
-    const connection = await mysql.createConnection("mysql://root:Lorde_2002@localhost:3306/dbmoney")
+    const connection = await mysql.createConnection("mysql://root:password@localhost:3306/dbmoney")
     global.connection = connection
     return connection
 }
